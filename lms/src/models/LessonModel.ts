c9a978d9a697149b6d953b7cfea58250bc1d6bb1
venkatalakshmimandalapu <132929092+venkatalakshmimandalapu@users.noreply.github.com>
2025-03-15 11:14:17 +0530

    Delete lms directory

--- lms/src/models/LessonModel.ts
+++
@@ -1,9 +0,0 @@
-export interface Lesson {
-    id: string;
-    title: string;
-    description: string;
-    fileUrl: string;
-    uploadedBy: string; // "Teacher" or "Student"
-    uploadedAt: Date;
-  }
-  
